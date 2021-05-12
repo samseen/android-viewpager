0ccf0c3aceb46653f4acec3d3cc05082b58136a5
Samson Akanbi <samson.akanbi@outlook.com>
2021-05-12 08:27:33 +0100

    Sliding through the slider now shows each of the page.

---
+++ app/src/main/java/com/example/viewpager/MainActivity.java
@@ -0,0 +1,103 @@
+package com.example.viewpager;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+import androidx.viewpager2.widget.ViewPager2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    private OnboardingAdapter onboardingAdapter;
+    private LinearLayout layoutOnboardingIndicators;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        layoutOnboardingIndicators = findViewById(R.id.layoutOnboardingIndicators);
+
+        setupOnboardingItems();
+
+        ViewPager2 onboardingViewPager = findViewById(R.id.onboardingViewPager);
+        onboardingViewPager.setAdapter(onboardingAdapter);
+
+        setupOnboardingIndicators();
+        setCurrentOnboardingIndicator(0);
+
+        onboardingViewPager.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                super.onPageSelected(position);
+                setCurrentOnboardingIndicator(position);
+            }
+        });
+    }
+
+    private void setupOnboardingItems() {
+
+        List<OnboardingItem> onboardingItems = new ArrayList<>();
+
+        OnboardingItem itemPayOnline = new OnboardingItem();
+        itemPayOnline.setTitle("Pay Your Bill Online");
+        itemPayOnline.setDescription("Electric bill payment is a feature of online, mobile and telephone banking.");
+        itemPayOnline.setImage(R.drawable.eat);
+
+        OnboardingItem itemOnTheWay = new OnboardingItem();
+        itemOnTheWay.setTitle("Your Food is on the way");
+        itemOnTheWay.setDescription("Our delivery rider is on the way to deliver your order.");
+        itemOnTheWay.setImage(R.drawable.talk);
+
+        OnboardingItem itemEatTogether = new OnboardingItem();
+        itemEatTogether.setTitle("Eat Together");
+        itemEatTogether.setTitle("Eat Alone");
+        itemEatTogether.setDescription("Enjoy your iftar and have great day. Don't forget to rate us.");
+        itemEatTogether.setImage(R.drawable.chop);
+
+        onboardingItems.add(itemPayOnline);
+        onboardingItems.add(itemOnTheWay);
+        onboardingItems.add(itemEatTogether);
+
+        onboardingAdapter = new OnboardingAdapter(onboardingItems);
+    }
+
+    private void setupOnboardingIndicators() {
+        ImageView[] indicators = new ImageView[onboardingAdapter.getItemCount()];
+        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(
+                ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT
+        );
+        layoutParams.setMargins(8, 0, 8, 0);
+        for(int i = 0; i < indicators.length; i++) {
+            indicators[i] = new ImageView(getApplicationContext());
+            indicators[i].setImageDrawable(ContextCompat.getDrawable(
+                    getApplicationContext(),
+                    R.drawable.onboarding_indicator_inactive
+            ));
+            indicators[i].setLayoutParams(layoutParams);
+            layoutOnboardingIndicators.addView(indicators[i]);
+        }
+    }
+
+    private void setCurrentOnboardingIndicator(int index) {
+        int childCount = layoutOnboardingIndicators.getChildCount();
+        for (int i = 0; i < childCount; i++) {
+            ImageView imageView = (ImageView)layoutOnboardingIndicators.getChildAt(i);
+            if (i == index) {
+                imageView.setImageDrawable(
+                        ContextCompat.getDrawable(getApplicationContext(), R.drawable.onboarding_indicator_active)
+                );
+            } else {
+                imageView.setImageDrawable(
+                        ContextCompat.getDrawable(getApplicationContext(), R.drawable.onboarding_indicator_inactive)
+                );
+            }
+        }
+    }
+}
